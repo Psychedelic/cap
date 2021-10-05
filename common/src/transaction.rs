c531e305cf13bdc2787a6723c6692631a9eda433
amber <amber@fleek.co>
2021-10-04 22:21:10 -0500

    Revert changes to `Event` and `IndefiniteEvent`.

--- common/src/transaction.rs
+++ common/src/transaction.rs
@@ -5,66 +5,53 @@ use serde::Serialize;
 use sha2::Digest;
 use std::collections::BTreeSet;
 
-/// An event that took place in the transaction history of a token.
-///
-/// The main difference between this type and [`IndefiniteEvent`] is that
-/// this event happened at a defined point in time.
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
 pub struct Event {
     /// The timestamp in ms.
-    time: u64,
+    pub time: u64,
     /// The caller that initiated the call on the token contract.
-    caller: Principal,
+    pub caller: Principal,
     /// The amount of tokens that was touched in this event.
-    amount: u64,
+    pub amount: u64,
     /// The fee captured by the token contract.
-    fee: u64,
+    pub fee: u64,
     /// The transaction memo.
-    memo: u32,
-    /// The source of this transaction.
-    ///
-    /// Must not be [`None` if the `operation` is [`Operation::Transfer`].
-    from: Option<Principal>,
+    pub memo: u32,
+    /// The `from` field, only needs to be non-null for transferFrom kind of events.
+    pub from: Option<Principal>,
     /// The receiver end of this transaction.
-    to: Principal,
+    pub to: Principal,
     /// The operation that took place.
-    operation: Operation,
+    pub operation: Operation,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub enum Operation {
+    Transfer,
+    Approve,
+    Mint,
+    Burn,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub struct IndefiniteEvent {
+    /// The caller that initiated the call on the token contract.
+    pub caller: Principal,
+    /// The amount of tokens that was touched in this event.
+    pub amount: u64,
+    /// The fee captured by the token contract.
+    pub fee: u64,
+    /// The transaction memo.
+    pub memo: u32,
+    /// The `from` field, only needs to be non-null for transferFrom kind of events.
+    pub from: Option<Principal>,
+    /// The receiver end of this transaction.
+    pub to: Principal,
+    /// The operation that took place.
+    pub operation: Operation,
 }
 
 impl Event {
-    /// Creates a new [`Event`]
-    ///
-    /// # Panics
-    /// Panics if the `operation` is [`Operation::Transfer`] and `from` is [`None`].
-    #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        time: u64,
-        caller: Principal,
-        amount: u64,
-        fee: u64,
-        memo: u32,
-        from: Option<Principal>,
-        to: Principal,
-        operation: Operation,
-    ) -> Self {
-        // Since the specification states that all Operation::Transfer must have a source, we panic
-        // if the caller has not provided one.
-        if operation == Operation::Transfer && from.is_none() {
-            panic!("A transfer operation must have a source.");
-        } else {
-            Self {
-                time,
-                caller,
-                amount,
-                fee,
-                memo,
-                from,
-                to,
-                operation,
-            }
-        }
-    }
-
     /// Return a set containing all of the Principal IDs involved in an event.
     #[inline]
     pub fn extract_principal_ids(&self) -> BTreeSet<&Principal> {
@@ -102,109 +89,10 @@ impl Event {
 
         h.finalize().into()
     }
-
-    #[inline]
-    pub fn time(&self) -> u64 {
-        self.time
-    }
-
-    #[inline]
-    pub fn caller(&self) -> Principal {
-        self.caller
-    }
-
-    #[inline]
-    pub fn amount(&self) -> u64 {
-        self.amount
-    }
-
-    #[inline]
-    pub fn fee(&self) -> u64 {
-        self.fee
-    }
-
-    #[inline]
-    pub fn memo(&self) -> u32 {
-        self.memo
-    }
-
-    #[inline]
-    pub fn from(&self) -> Option<Principal> {
-        self.from
-    }
-
-    #[inline]
-    pub fn to(&self) -> Principal {
-        self.to
-    }
-
-    #[inline]
-    pub fn operation(&self) -> Operation {
-        self.operation
-    }
-}
-
-#[derive(CandidType, Serialize, Deserialize, Clone, Eq, PartialEq, Copy, Debug)]
-pub enum Operation {
-    Transfer,
-    Approve,
-    Mint,
-    Burn,
-}
-
-/// An event that doesn't have a defined timestamp.
-#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
-pub struct IndefiniteEvent {
-    /// The caller that initiated the call on the token contract.
-    caller: Principal,
-    /// The amount of tokens that was touched in this event.
-    amount: u64,
-    /// The fee captured by the token contract.
-    fee: u64,
-    /// The transaction memo.
-    memo: u32,
-    /// The source for the transaction.
-    ///
-    /// Must not be [`None`] if the `operation` is [`Operation::Transfer`].
-    from: Option<Principal>,
-    /// The receiver end of this transaction.
-    to: Principal,
-    /// The operation that took place.
-    operation: Operation,
 }
 
 impl IndefiniteEvent {
-    /// Creates a new [`IndefiniteEvent`]
-    ///
-    /// # Panics
-    /// Panics if the `operation` is [`Operation::Transfer`] and `from` is [`None`].
-    pub fn new(
-        caller: Principal,
-        amount: u64,
-        fee: u64,
-        memo: u32,
-        from: Option<Principal>,
-        to: Principal,
-        operation: Operation,
-    ) -> Self {
-        // Since the specification states that all Operation::Transfer must have a source, we panic
-        // if the caller has not provided one.
-        if operation == Operation::Transfer && from.is_none() {
-            panic!("A transfer operation must have a source.");
-        } else {
-            Self {
-                caller,
-                amount,
-                fee,
-                memo,
-                from,
-                to,
-                operation,
-            }
-        }
-    }
-
-    // Convert an indefinite event to a definite one by adding the token and time fields.
+    /// Convert an indefinite event to a definite one by adding the token and time fields.
     #[inline]
     pub fn to_event(self, time: u64) -> Event {
         Event {
@@ -217,41 +105,6 @@ impl IndefiniteEvent {
             to: self.to,
             operation: self.operation,
         }
-    }
-
-    #[inline]
-    pub fn caller(&self) -> Principal {
-        self.caller
-    }
-
-    #[inline]
-    pub fn amount(&self) -> u64 {
-        self.amount
-    }
-
-    #[inline]
-    pub fn fee(&self) -> u64 {
-        self.fee
-    }
-
-    #[inline]
-    pub fn memo(&self) -> u32 {
-        self.memo
-    }
-
-    #[inline]
-    pub fn from(&self) -> Option<Principal> {
-        self.from
-    }
-
-    #[inline]
-    pub fn to(&self) -> Principal {
-        self.to
-    }
-
-    #[inline]
-    pub fn operation(&self) -> Operation {
-        self.operation
     }
 }
 
