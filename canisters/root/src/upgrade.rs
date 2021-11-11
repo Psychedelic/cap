b8cd807f0ef35ac5afa5fdbb3a34fafe7cc5bf01
qti3e <parsa@fleek.co>
2021-11-11 18:41:35 +0330

    implement the upgrade code

--- canisters/root/src/upgrade.rs
+++ canisters/root/src/upgrade.rs
@@ -1,11 +1,18 @@
 use crate::Data;
-use ic_cdk::api::stable::StableWriter;
+use ic_cdk::api::stable::{StableReader, StableWriter};
 use ic_kit::ic;
-use ic_kit::macros::pre_upgrade;
+use ic_kit::macros::{pre_upgrade, post_upgrade};
 
 #[pre_upgrade]
 fn pre_upgrade() {
     let data = ic::get::<Data>();
     let writer = StableWriter::default();
     serde_cbor::to_writer(writer, &data).expect("Failed to serialize data.");
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    let reader = StableReader::default();
+    let data = serde_cbor::from_reader(reader).expect("Failed to deserialize");
+    ic::store::<Data>(data);
 }
