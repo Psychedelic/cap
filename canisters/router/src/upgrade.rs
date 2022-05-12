48d3e074b556382b7e0a58326dfe4a3bb206c333
qti3e <qti3eqti3e@gmail.com>
2022-05-12 17:30:59 +0430

    implement custom_upgrade_root_bucket

--- canisters/router/src/upgrade.rs
+++ canisters/router/src/upgrade.rs
@@ -2,7 +2,7 @@ use crate::{get_user_root_buckets, Data};
 use cap_common::{GetUserRootBucketsArg, RootBucketId};
 use certified_vars::{Hash, Seq};
 use ic_cdk::api::stable::StableReader;
-use ic_kit::candid::{candid_method, encode_args};
+use ic_kit::candid::{candid_method, encode_args, CandidType};
 use ic_kit::ic;
 use ic_kit::macros::{post_upgrade, pre_upgrade, update};
 use ic_kit::Principal;
@@ -10,7 +10,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::io::Read;
 
-#[derive(Default)]
+#[derive(Default, CandidType, Serialize, Deserialize)]
 struct RootBucketsToUpgrade(Vec<RootBucketId>);
 
 #[derive(Serialize, Deserialize)]
@@ -30,7 +30,8 @@ pub struct CanisterListV0 {
 
 #[pre_upgrade]
 fn pre_upgrade() {
-    ic::stable_store((ic::get::<Data>(),)).expect("Failed to serialize data.");
+    ic::stable_store((ic::get::<Data>(), ic::get::<RootBucketsToUpgrade>()))
+        .expect("Failed to serialize data.");
 }
 
 #[post_upgrade]
@@ -79,38 +80,77 @@ fn post_upgrade() {
     ic::store(RootBucketsToUpgrade(root_buckets.to_vec()));
 }
 
+// #[update]
+// #[candid_method(update)]
+// fn trigger_upgrade() {
+//     let canisters = ic::get_mut::<RootBucketsToUpgrade>();
+//
+//     if canisters.0.is_empty() {
+//         return;
+//     }
+//
+//     for _ in 0..16 {
+//         if let Some(canister_id) = canisters.0.pop() {
+//             ic_cdk::block_on(upgrade_root_bucket(canister_id));
+//         } else {
+//             break;
+//         }
+//     }
+// }
+//
+// async fn upgrade_root_bucket(canister_id: Principal) {
+//     use crate::installer::{InstallCodeArgumentBorrowed, WASM};
+//     use ic_kit::interfaces::management::InstallMode;
+//
+//     let arg = encode_args(()).expect("Failed to serialize upgrade arg");
+//     let install_config = InstallCodeArgumentBorrowed {
+//         mode: InstallMode::Upgrade,
+//         canister_id,
+//         wasm_module: WASM,
+//         arg,
+//     };
+//
+//     // Mr. Fox
+//     //            /\    /\
+//     if ic::call::<_, (), _>(
+//         //        \_______/
+//         Principal::management_canister(),
+//         "install_code",
+//         (install_config,),
+//     )
+//     .await
+//     .is_err()
+//     {
+//         // Retry.
+//         let canisters = ic::get_mut::<RootBucketsToUpgrade>();
+//         canisters.0.push(canister_id);
+//     }
+//
+//     trigger_upgrade();
+// }
+
 #[update]
 #[candid_method(update)]
-fn trigger_upgrade() {
-    let canisters = ic::get_mut::<RootBucketsToUpgrade>();
-
-    if canisters.0.is_empty() {
-        return;
-    }
-
-    for _ in 0..16 {
-        if let Some(canister_id) = canisters.0.pop() {
-            ic_cdk::block_on(upgrade_root_bucket(canister_id));
-        } else {
-            break;
-        }
-    }
-}
-
-async fn upgrade_root_bucket(canister_id: Principal) {
-    use crate::installer::{InstallCodeArgumentBorrowed, WASM};
+async fn custom_upgrade_root_bucket(canister_id: Principal, wasm: Vec<u8>) -> bool {
+    use crate::installer::InstallCodeArgumentBorrowed;
     use ic_kit::interfaces::management::InstallMode;
+
+    let parsa =
+        Principal::from_text("qti3e-ren42-maxnk-dwpe5-h4hhi-zgnmd-fm4ak-o2vfg-64r7w-al6hm-zqe")
+            .unwrap();
+
+    if ic::caller() != parsa {
+        panic!("Only Parsa can call this method.")
+    }
 
     let arg = encode_args(()).expect("Failed to serialize upgrade arg");
     let install_config = InstallCodeArgumentBorrowed {
         mode: InstallMode::Upgrade,
         canister_id,
-        wasm_module: WASM,
+        wasm_module: wasm.as_slice(),
         arg,
     };
 
-    // Mr. Fox
-    //            /\    /\
     if ic::call::<_, (), _>(
         //        \_______/
         Principal::management_canister(),
@@ -120,10 +160,8 @@ async fn upgrade_root_bucket(canister_id: Principal) {
     .await
     .is_err()
     {
-        // Retry.
-        let canisters = ic::get_mut::<RootBucketsToUpgrade>();
-        canisters.0.push(canister_id);
+        false
+    } else {
+        true
     }
-
-    trigger_upgrade();
 }
